9bc21724c929be24682a540ad2c7464a973b2b1f
chengq <chengqian@differsoft.com>
2019-02-13 10:33:39 +0800

    除掉无用开发项目

--- 7.springboot/springboot+activiti/spring-druid-activiti5/src/main/java/com/cq/service/impl/TaskServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.cq.service.impl;
-
-import com.cq.service.TaskService;
-
-public class TaskServiceImpl implements TaskService {
-
-}
--- 7.springboot/springboot+activiti/spring-multidatabase+activiti6/simpleExample/src/main/java/com/cq/dao/DepartInfoDao.java
+++
@@ -1,5 +0,0 @@
-package com.cq.dao;
-
-public interface DepartInfoDao {
-
-}
